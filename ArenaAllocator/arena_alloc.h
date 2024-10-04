d57fae1c01d692389ac963a050edc05e2ae5e665
Fazel Montazery <ryan.montazery@protonmail.com>
2024-10-04 17:07:20 +0330

    Add arena_alloc and arena_flush impelementation
    Implementations do not currently account for memory alignment of returned memory.
    Test files have been updated accordingly.

--- ArenaAllocator/arena_alloc.h
+++ ArenaAllocator/arena_alloc.h
@@ -19,6 +19,8 @@ void arena_flush(struct Arena* arena);
 // Api implementation
 #ifdef ARENA_ALLOC_IMPELEMENTATION
 
+#include <stdio.h>
+
 struct Arena arena_create(byte* buffer, size_t size)
 {
 	return (struct Arena) {
@@ -28,5 +30,21 @@ struct Arena arena_create(byte* buffer, size_t size)
 	};
 }
 
+void* arena_allocate(struct Arena* arena, size_t size)
+{
+	if (arena->offset + size > arena->capacity) {
+		fprintf(stderr, "Arena out of space! [capacity => %lu]\n", arena->capacity);
+		return NULL;
+	}
+
+	void* tmp = arena->buff + arena->offset;
+	arena->offset += size;
+	return tmp;
+}
+
+void arena_flush(struct Arena* arena)
+{
+	arena->offset = 0;
+}
 
 #endif // ARENA_ALLOC_IMPELEMENTATION
--- Test/ArenaAllocator/test.c
+++ Test/ArenaAllocator/test.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <assert.h>
 
 #define BUFF_SIZE 1000
@@ -17,9 +18,33 @@ int main(int argc, char** argv)
 	assert(arena.buff == buffer);
 	assert(arena.capacity == BUFF_SIZE);
 	assert(arena.offset == 0);
-	printf("Arena Creation OK.");
+	printf("Arena Creation OK.\n");
 
+	int32_t* arr1 = arena_allocate(&arena, sizeof(int32_t) * 100);
+	assert(arr1 != NULL);
+	assert(arena.offset == (sizeof(int32_t) * 100));
+	printf("Arena Allocation[1] OK.\n");
 
+	int32_t* arr2 = arena_allocate(&arena, sizeof(int32_t) * 150);
+	assert(arr2 != NULL);
+	assert(arena.offset == (sizeof(int32_t) * 250));
+	printf("Arena Allocation[2] OK.\n");
+
+	int32_t* arr3 = arena_allocate(&arena, sizeof(int32_t));
+	assert(arr3 == NULL);
+	assert(arena.offset == (sizeof(int32_t) * 250));
+	printf("Arena Allocation[3] OK.\n");
+
+	arena_flush(&arena);
+	assert(arena.offset == 0);
+	printf("Arena Flush OK.\n");
+
+	int32_t* arr4 = arena_allocate(&arena, sizeof(int32_t) * 250);
+	assert(arr4 != NULL);
+	assert(arena.offset == (sizeof(int32_t) * 250));
+	printf("Arena Allocation[4] OK.\n");
+
+	printf("Everything OK.\n");
 
 	free(buffer);
 	return 0;
