bb65963bf2d982d86c4ed8e9a1f8e0a0fd8db0c4
Fazel Montazery <ryan.montazery@protonmail.com>
2024-10-04 15:37:18 +0330

    Add api declarations and Arena structure

--- ArenaAllocator/arena_alloc.h
+++ ArenaAllocator/arena_alloc.h
@@ -1,7 +1,18 @@
 #pragma once
 
-// Api declaration
+typedef unsigned char byte;
 
+// Arena structure
+struct Arena {
+	byte* buff;
+	size_t offset;
+	size_t capacity;
+};
+
+// Api declaration
+struct Arena arena_create(byte* buffer, size_t size);
+void* arena_allocate(struct Arena* arena, size_t size);
+void arena_flush(struct Arena* arena);
 
 // Api implementation
 #ifdef ARENA_ALLOC_IMPELEMENTATION
