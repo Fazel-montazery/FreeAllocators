dffbc8ebbfe2b8338cf7e61cf6a8948084bb3218
Fazel Montazery <ryan.montazery@protonmail.com>
2024-10-13 23:30:21 +0330

    Add pool_create and test file updated
    Regulates the layout for chunks in the buffer and return's true
    if it was successful otherwise false.
    test file updated acordingly.

--- PoolAllocator/pool_alloc.h
+++ PoolAllocator/pool_alloc.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <stdint.h>
+#include <stdbool.h>
+
 typedef unsigned char byte;
 
 // Header structure
 struct ChunkHeader {
-	struct Header* nextFree;
+	struct ChunkHeader* nextFree;
 };
 
 // Pool structure
@@ -17,25 +20,29 @@ struct Pool {
 };
 
 // Api declaration
-struct Pool pool_create(byte* buffer, int64_t bufferSize, int64_t chunkSize, bool initZero);
+bool pool_create_align(struct Pool* p, byte* buffer, int64_t bufferSize, int64_t chunkSize, uintptr_t chunkAlignment, bool initZero);
+bool pool_create(struct Pool* p, byte* buffer, int64_t bufferSize, int64_t chunkSize, bool initZero);
+void pool_flush(struct Pool* pool);
 
 // Api implementation
 #ifdef POOL_ALLOC_IMPELEMENTATION
 
 #ifndef DEFAULT_ALIGNMENT
-#define DEFAULT_ALIGNMENT (2 * sizeof(void *))
+#define DEFAULT_ALIGNMENT 8
 #endif
 
 #ifndef POOL_ALLOC_NO_LOG
 #include <stdio.h>
 #endif
 
-static uintptr_t align_forward(uintptr_t ptr, int64_t align) 
+#include <string.h>
+
+static uintptr_t align_forward(uintptr_t ptr, uintptr_t align) 
 {
 	uintptr_t p, a, modulo;
 
 	p = ptr;
-	a = (uintptr_t) align;
+	a = align;
 	modulo = p & (a - 1);
 
 	if (modulo != 0) {
@@ -43,6 +50,52 @@ static uintptr_t align_forward(uintptr_t ptr, int64_t align)
 	}
 
 	return p;
+}
+
+bool pool_create_align(struct Pool* p, byte* buffer, int64_t bufferSize, int64_t chunkSize, uintptr_t chunkAlignment, bool initZero)
+{
+	uintptr_t initStart = (uintptr_t) buffer;
+	uintptr_t start = align_forward(initStart, (uintptr_t) chunkAlignment);
+	bufferSize -= (int64_t) (start - initStart);
+
+	chunkSize = align_forward((uintptr_t)chunkSize, chunkAlignment);
+	if (chunkSize < sizeof(struct ChunkHeader)) {
+#ifndef POOL_ALLOC_NO_LOG
+		fprintf(stderr, "Chunk size is too small.\n");
+#endif
+		return false;
+	}
+
+	if (bufferSize < chunkSize) {
+#ifndef POOL_ALLOC_NO_LOG
+		fprintf(stderr, "Buffer size is too small.\n");
+#endif
+		return false;
+	}
+
+	if (initZero) memset(buffer, 0, bufferSize);
+
+	p->buff = (byte*) buffer;
+	p->capacity = bufferSize;
+	p->chunkSize = chunkSize;
+	p->head = NULL;
+
+	pool_flush(p);
+}
+
+bool pool_create(struct Pool* p, byte* buffer, int64_t bufferSize, int64_t chunkSize, bool initZero) {
+	return pool_create_align(p, buffer, bufferSize, chunkSize, DEFAULT_ALIGNMENT, initZero);
+}
+
+void pool_flush(struct Pool* pool)
+{
+	int64_t chunkCount = pool->capacity / pool->chunkSize;
+	for (int64_t i = 0; i < chunkCount; i++) {
+		void* ptr = &pool->buff[i * pool->chunkSize];
+		struct ChunkHeader* header = (struct ChunkHeader*) ptr;
+		header->nextFree = pool->head;
+		pool->head = header;
+	}
 }
 
 #endif // POOL_ALLOC_IMPELEMENTATION
